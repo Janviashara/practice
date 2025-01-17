a3bdecadbd1b7d6c7a262f9808c82c9897c09954
--global <janvi.a@devtime.in>
2025-01-17 19:01:11 +0530

    Flatten Nested Object

--- script5.js
+++ script5.js
@@ -1,3 +1,25 @@
 //5. Flatten Nested Object
 
+function flattenObject(obj){
+    let output = {};
+    for(let key in obj){
+        if(typeof obj[key] === 'object'){
+            let flatObject= flattenObject(obj[key]);
+            for(let flatKey in flatObject){
+                output[key + '.'+ flatKey] = flatObject[flatKey];
+            }
+        }
+        else{
+            output[key] = obj[key];
+        }
+    }
+    return output;
+}
+const details= flattenObject({
+    user: {
+        name: "Alice",
+        address: { city: "Wonderland", zip: "12345" },
+    }
+});
+console.log(details);
 
