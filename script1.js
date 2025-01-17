4e063bfbe8282b363b0ebbbfc76edc1bdbd2a642
--global <janvi.a@devtime.in>
2025-01-17 17:31:05 +0530

    BuildQueryString

--- script1.js
+++ script1.js
@@ -8,7 +8,7 @@
   function buildQueryString(obj){
     let keys = Object.keys(obj);
     let values = Object.values(obj);
-    let queryString = "";
+    let queryString = "?";
     for(let i=0; i<keys.length; i++){
       queryString += `${keys[i]}=${values[i]}&`;      
     }
