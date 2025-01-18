e82910517780b1232a3be1f0b1d8db14e583c740
Janviashara <janvi.a@devtime.in>
2025-01-18 18:51:50 +0530

    Longestpalindrom number

--- script7.js
+++ script7.js
@@ -1,8 +1,25 @@
 //Validate Balanced Brackets
 //Write a function isBalanced that checks if a string has balanced brackets (e.g., {}, [], ()).
 
+let isBalanced = (equation) => {
 
-function isBalanced(string){
-    
-    
-}
+    let brackets = "[]{}()<>"
+    let stack = []
+  
+    for(let bracket of equation) {
+      let bracketsIndex = brackets.indexOf(bracket)
+  
+      if(bracketsIndex % 2 === 0) {
+        stack.push(bracketsIndex + 1)
+      } else {
+        if(stack.pop() !== bracketsIndex) {
+          return false;
+        }
+      }
+    }
+    return stack.length === 0
+  }
+  
+ console.log(isBalanced('({<>})'))
+  
+ 
--- script9.js
+++ script9.js
@@ -1,8 +1,26 @@
 //9. Find Longest Palindrome
 
-//Write a function findLongestPalindrome to find the longest palindrome in a given string.
-
-function findLongestPalindrome(string) {
-    
-}
-findLongestPalindrome("babad");
+function isPalindrome(str) {
+    return str === str.split('').reverse().join('');
+  }
+  
+  function findLongestPalindrome(str) {
+    let maxLength = 0;         
+    let longestPalindrome = ''; 
+  
+    for (let i = 0; i < str.length; i++) {
+      for (let j = i + 1; j <= str.length; j++) {
+        let substring = str.slice(i, j); // Extract the current substring
+         
+        if (isPalindrome(substring) && substring.length > maxLength) {
+          maxLength = substring.length;        
+          longestPalindrome = substring;       
+        }
+      }
+    }
+  
+    return longestPalindrome; 
+  }
+  
+  console.log(findLongestPalindrome("123abcdcba12321abcdefghgfedcba"));
+  
